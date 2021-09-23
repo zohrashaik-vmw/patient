e63693b98708400fc4296ff4228a9901d1836ac7
zohrashaik-vmw <71278998+zohrashaik-vmw@users.noreply.github.com>
2021-09-23 07:53:47 -0500

    removed references

--- src/main/java/com/livewell/patient/PatientApplication.java
+++ src/main/java/com/livewell/patient/PatientApplication.java
@@ -1,10 +1,8 @@
 package com.livewell.patient;
 
-import com.livewell.patient.model.Patient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
+
 
 @SpringBootApplication
 public class PatientApplication {
